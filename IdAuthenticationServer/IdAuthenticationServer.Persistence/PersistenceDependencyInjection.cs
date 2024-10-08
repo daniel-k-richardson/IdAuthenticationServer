b6884cb6d5e74b2bb1494e9530a78d429ab35730
DanielRichardson <daniel@vigilo.no>
2024-10-08 14:57:45 +0200

    add dependencyInject for persistence layer

--- IdAuthenticationServer/IdAuthenticationServer.Persistence/PersistenceDependencyInjection.cs
+++ IdAuthenticationServer/IdAuthenticationServer.Persistence/PersistenceDependencyInjection.cs
@@ -1,6 +1,22 @@
+using IdAuthenticationServer.Persistence.ContextConfiguration;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
 namespace IdAuthenticationServer.Persistence;
 
-public class PersistenceDependencyInjection
+public static class PersistenceDependencyInjection
 {
-    
+    public static IServiceCollection AddPersistence(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddDbContext<IdAuthContext>((sp, options) =>
+        {
+            options.UseSqlServer(configuration.GetConnectionString("DefaultConnection"));
+        });
+
+        services.AddScoped<IdAuthContext>(provider => provider.GetRequiredService<IdAuthContext>());
+        // services.AddHostedService<DatabaseMigrationService>();
+
+        return services;
+    }
 }
